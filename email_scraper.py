a048f7128fbe85958017b2e7e42bd1963c174bed
Z333RO <82729059+Z333RO@users.noreply.github.com>
2022-11-24 16:29:42 -0800

    Update email_scraper.py

--- email_scraper.py
+++ email_scraper.py
@@ -5,7 +5,7 @@ import urllib.parse
 from collections import deque
 import re
 
-user_url = str(input('[+] Enter URL list to scan: '))
+user_url = str(input('[+] Enter URL to scan: '))
 urls = deque([user_url])
 
 scraped_urls = set()
@@ -26,10 +26,26 @@ try:
         path = url[:url.rfind('/') + 1] if '/' in parts.path else url
 
         print('[%d] Processing %s' % (count,url))
-            try:
-                response = requests.get(url)
-            except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
-                continue
+        try:
+            response = requests.get(url)
+        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+            continue
 
-            new_emails = set(re.findall(r" [a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+"), response.text, re.I))
-            emails.update(new_emails)
+        new_emails = set(re.findall(r" [a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
+        emails.update(new_emails)
+
+        soup = BeautifulSoup(response.text, features="lxml")
+
+        for anchor in soup.find_all('a'):
+            link = anchor.attrs('href') if 'href' in anchor.attrs else ''
+            if link.startswith('/'):
+                link = base_url + link
+            elif not link.startswith('http'):
+                link = path + link
+            if not link in urls and not link in scraped_urls:
+                urls.append(link)
+except KeyboardInterrupt:
+    print('[+] Cancelled.')
+
+for mail in emails:
+    print(mail)
