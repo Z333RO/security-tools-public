64ed1213b49946e7cb9a8d6ee4114a164fb00aad
Z333RO <82729059+Z333RO@users.noreply.github.com>
2023-05-30 16:32:28 -0700

    Create spider_python3.py
    Python3 Website Crawler to recursively grab URL links from a domain.

---
+++ spider_python3.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# Python3 Website Spider Script
+
+import requests
+import re
+import urllib.parse as urlparse
+
+
+target_url = "http://192.0.0.2/mutillidae/" # CHANGE THIS
+target_links = []
+
+def extract_links_from(url):
+    response = requests.get(url)
+    return re.findall('(?:href=")(.*?)"', response.content.decode(errors="decode"))
+
+def crawl(url):
+    href_links = extract_links_from(url)
+    for link in href_links:
+        link = urlparse.urljoin(url, link)
+
+        if "#" in link:
+            link = link.split("#")[0]
+
+        if target_url in link and link not in target_links:
+            target_links.append(link)
+            print(link)
+            crawl(link)
+
+crawl(target_url)
