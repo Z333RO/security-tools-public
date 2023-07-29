b56ad657a8cce6ef5977bcf59641df66e997f9d8
Matt Manalac <82729059+Z333RO@users.noreply.github.com>
2023-07-29 11:54:05 -0700

    Update 2nd_update_install.sh

--- kali_autosetup/2nd_update_install.sh
+++ kali_autosetup/2nd_update_install.sh
@@ -14,6 +14,7 @@ install_package nuclei
 install_package seclists
 install_package gedit
 install_package tor
+install_package rlwrap
 install_package bloodhound
 
 dpkg -i /home/kali/tools/rustscan_2.0.1_amd64.deb
