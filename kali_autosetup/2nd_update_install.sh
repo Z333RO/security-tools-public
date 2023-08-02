98c4fac7901d9179ed536fdd75f6eba70793946f
Matt Manalac <82729059+Z333RO@users.noreply.github.com>
2023-08-02 15:52:00 -0700

    Update 2nd_update_install.sh

--- kali_autosetup/2nd_update_install.sh
+++ kali_autosetup/2nd_update_install.sh
@@ -20,4 +20,13 @@ install_package bloodhound
 dpkg -i /home/kali/tools/rustscan_2.0.1_amd64.deb
 
 searchsploit -u
+
+# windows-exploit-suggester setup and dependencies install
+wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+python2 get-pip.py
+rm get-pip.py
+pip2 install xlrd==1.2.0
+# Go into the windows exploit suggester folder and run the following to setup exploit-suggester
+# python2 windows-exploit-suggester.py --update
+
 updatedb
