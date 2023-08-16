298d022f483cad6eb3a6f7c0611c7e30471ac6c1
Matt Manalac <82729059+Z333RO@users.noreply.github.com>
2023-08-15 21:18:37 -0400

    Update 2nd_update_install.sh

--- kali_autosetup/2nd_update_install.sh
+++ kali_autosetup/2nd_update_install.sh
@@ -18,6 +18,7 @@ install_package rlwrap
 install_package eyewitness
 install_package bloodhound
 install_package trufflehog
+install_package dirsearch
 
 dpkg -i /home/kali/tools/rustscan_2.0.1_amd64.deb
 
