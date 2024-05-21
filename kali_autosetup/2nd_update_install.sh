d9df6b03f424f346ba80935533e80db9299a58ff
Matt Manalac <82729059+Z333RO@users.noreply.github.com>
2024-05-21 15:19:25 -0700

    Update 2nd_update_install.sh

--- kali_autosetup/2nd_update_install.sh
+++ kali_autosetup/2nd_update_install.sh
@@ -43,6 +43,7 @@ install_package commix
 install_package libreoffice
 install_package spray
 install_package ntpdate
+install_package golang-go
 
 dpkg -i /home/kali/tools/rustscan_2.0.1_amd64.deb
 
